22b290a5aee3041d6eddfd0176cc73d13552f390
VanY <apple@APPLEs-MacBook-Pro.local>
2024-02-16 14:56:30 +0200

    [finisjed]: finished pages

---
+++ src/scripts/dashboard.js
@@ -0,0 +1,35 @@
+const accordion = document.getElementsByClassName("container");
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active')
+
+    })
+}
+
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = x.previousElementSibling.className.replace(
+            " w3-green",
+            ""
+        );
+    }
+}
+
+function myDropFunc() {
+    var x = document.getElementById("demoDrop");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = x.previousElementSibling.className.replace(
+            " w3-green",
+            ""
+        );
+    }
+}
